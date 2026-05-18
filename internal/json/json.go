1838601df2f1db75dffafcae109a40dc857761eb
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-05-18 12:02:25 +0800

    feat: add JSON utility functions for reading and writing data

--- internal/json/json.go
+++ internal/json/json.go
@@ -3,7 +3,11 @@ package json
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
+
+	"github.com/LuisCabantac/portfolyo-go-api/internal/apperrors"
 )
 
 func Write(w http.ResponseWriter, status int, data any) error {
@@ -18,4 +22,28 @@ func Write(w http.ResponseWriter, status int, data any) error {
 	w.Write(buf.Bytes())
 
 	return nil
+}
+
+func WriteErrorResponse(w http.ResponseWriter, err error) error {
+	appErr, ok := errors.AsType[*apperrors.AppError](err)
+	if !ok {
+		return Write(w, apperrors.ErrInternalServerError.StatusCode, apperrors.ErrInternalServerError)
+	}
+
+	return Write(w, appErr.StatusCode, appErr)
+}
+
+func WriteJSON(w io.Writer, data any) error {
+	return json.NewEncoder(w).Encode(data)
+}
+
+func Read(r *http.Request, data any) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	return decoder.Decode(data)
+}
+
+func ReadJSON(w *http.Response, data any) error {
+	return json.NewDecoder(w.Body).Decode(data)
 }
