bf219d7d1c63b8a92e505505abcd40c7871794a3
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-05-20 01:02:45 +0800

    chore: add generics to JSON utility functions instead of any type

--- internal/json/json.go
+++ internal/json/json.go
@@ -10,7 +10,7 @@ import (
 	"github.com/LuisCabantac/portfolyo-go-api/internal/apperrors"
 )
 
-func Write(w http.ResponseWriter, status int, data any) error {
+func Write[T comparable](w http.ResponseWriter, status int, data T) error {
 	var buf bytes.Buffer
 
 	if err := json.NewEncoder(&buf).Encode(data); err != nil {
@@ -33,17 +33,17 @@ func WriteErrorResponse(w http.ResponseWriter, err error) error {
 	return Write(w, appErr.StatusCode, appErr)
 }
 
-func WriteJSON(w io.Writer, data any) error {
+func WriteJSON[T comparable](w io.Writer, data T) error {
 	return json.NewEncoder(w).Encode(data)
 }
 
-func Read(r *http.Request, data any) error {
+func Read[T comparable](r *http.Request, data T) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
 
 	return decoder.Decode(data)
 }
 
-func ReadJSON(resp *http.Response, data any) error {
+func ReadJSON[T comparable](resp *http.Response, data T) error {
 	return json.NewDecoder(resp.Body).Decode(data)
 }
